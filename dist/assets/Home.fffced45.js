f4f77cdd3cbf99c85afcfa6770280bc56294b24d
Juanma Cano <juanmacano@gmail.com>
2022-02-05 09:43:33 +0100

    new build

---
+++ dist/assets/Home.fffced45.js
@@ -0,0 +1,1 @@
+import{u as t}from"./counter.c73eb9b0.js";import{j as s,c as a,d as n,t as o,u as r,F as u,o as c}from"./vendor.2bc04838.js";const _=s({setup(l){const e=t();return(m,p)=>(c(),a(u,null,[n("h1",null,"Home: "+o(r(e).counter),1),n("h3",null,"number Of Changes: "+o(r(e).numberOfChanges),1)],64))}});export{_ as default};
---
+++ dist/assets/counter.c73eb9b0.js
@@ -0,0 +1,1 @@
+import{k as t}from"./vendor.2bc04838.js";const r=t("counter",{state:()=>({counter:0,numberOfChanges:0}),actions:{increment(e){this.counter+=e,this.numberOfChanges++}}});export{r as u};
